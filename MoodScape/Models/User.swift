92dcf44e39cf7203fc5f65abde60933ff50d320a
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-11 23:03:59 +0200

    user model added

---
+++ MoodScape/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 11.08.24.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let password: String
+    let email: String
+}
