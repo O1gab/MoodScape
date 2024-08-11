9acae90e3cacd1e0ddc29ac1a5371f786fdc3b5f
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-11 23:54:04 +0200

    LaunchView swiftUi style

--- MoodScape/Views/LaunchView.swift
+++ MoodScape/Views/LaunchView.swift
@@ -8,43 +8,35 @@ import SwiftUI
 
 struct LaunchView: View {
     @State private var isHidden = false
-    
+    @State private var navigateToLogin = false
+
     var body: some View {
         NavigationView {
-                    VStack {
-                        Spacer()
-                        Text("MoodScape")
-                            .font(.system(size: 37, weight: .bold))
-                            .foregroundColor(Color(red: 30/255, green: 215/255, blue: 96/255))
-                            .opacity(isHidden ? 0.0 : 1.0)
-                            .onAppear {
-                                withAnimation(Animation.easeInOut(duration: 3.0)) {
-                                    isHidden = true
-                                }
-                                // Delay the navigation to LoginView by the duration of the animation
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                                    navigateToLogin = true
-                                }
-                            }
-                        Spacer()
-                        // Navigate to LoginView when navigateToLogin is true
-                        NavigationLink(destination: LoginView(), isActive: $navigateToLogin) {
-                            EmptyView()
+            VStack {
+                Spacer()
+                Text("MoodScape")
+                    .font(.system(size: 37, weight: .bold))
+                    .foregroundColor(Color(red: 30/255, green: 215/255, blue: 96/255))
+                    .opacity(isHidden ? 0.0 : 1.0)
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 3.0)) {
+                            isHidden = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            navigateToLogin = true
                         }
                     }
-                    .background(Color.white)
-                    .edgesIgnoringSafeArea(.all)
-                }
+                Spacer()
             }
-}
-
-private extension LaunchView {
-    func nextView() {
-        let loginView = LoginView()
-        // go to the login view after launchview
-        
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.all)
+            .navigationDestination(isPresented: $navigateToLogin) {
+                LoginView()
+            }
+        }
     }
 }
+
 
 
 struct LaunchView_Previews: PreviewProvider {
