0cd19d6a20aca28a88feab3f7d708e26fbe51dc9
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-14 04:31:08 +0200

    MainBaseView: profile button integrated

--- MoodScape/Controllers/BaseViews/MainBaseView.swift
+++ MoodScape/Controllers/BaseViews/MainBaseView.swift
@@ -11,10 +11,12 @@ import Gifu
 class MainBaseView: UIViewController {
     
     let gifImageView = GIFImageView()
+    let profileButton = UIButton(type: .custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGIFBackground()
+        setupProfileButton()
         setupConstraints()
     }
     
@@ -26,12 +28,31 @@ class MainBaseView: UIViewController {
         view.sendSubviewToBack(gifImageView)
     }
     
+    private func setupProfileButton() {
+        profileButton.frame = CGRect(x: self.view.frame.width - 60, y: 40, width: 40, height: 40)
+        profileButton.layer.cornerRadius = 20
+        profileButton.clipsToBounds = true
+        profileButton.backgroundColor = .blue
+        profileButton.addTarget(self, action: #selector(profileTapped), for: .touchUpInside)
+        view.addSubview(profileButton)
+    }
+    
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             gifImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             gifImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             gifImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            gifImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            gifImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            profileButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 210),
+            profileButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
         ])
+    }
+    
+    @objc private func profileTapped() {
+        let profileView = MainViewController()
+        profileView.modalTransitionStyle = .crossDissolve
+        profileView.modalPresentationStyle = .fullScreen
+        self.present(profileView, animated: true, completion: nil)
     }
 }
