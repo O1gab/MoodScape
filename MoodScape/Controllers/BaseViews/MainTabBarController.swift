9772414f9de360e7d680a1a6302bf18288250ac8
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-15 16:46:35 +0200

    Main Bar Tab: swipe function added

--- MoodScape/Controllers/BaseViews/MainTabBarController.swift
+++ MoodScape/Controllers/BaseViews/MainTabBarController.swift
@@ -24,5 +24,28 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
         secondViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "2.circle"), tag: 1)
         thirdViewController.tabBarItem = UITabBarItem(title: "Stats", image: UIImage(systemName: "3.circle"), tag: 2)
         fourthViewController.tabBarItem = UITabBarItem(title: "Social", image: UIImage(systemName: "4.circle"), tag: 3)
+        
+        // - MARK: SWIPE GESTURES
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        if let currentIndex = viewControllers?.firstIndex(of: selectedViewController!) {
+            if gesture.direction == .left {
+                if currentIndex < (viewControllers?.count)! - 1 {
+                    selectedIndex = currentIndex + 1
+                }
+            } else if gesture.direction == .right {
+                if currentIndex > 0 {
+                    selectedIndex = currentIndex - 1
+                }
+            }
+        }
     }
 }
