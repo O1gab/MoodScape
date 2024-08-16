14be50b833c7dd3d9d77dae3f0ceca131d6b4a36
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-16 02:31:11 +0200

    Album item added

---
+++ MoodScape/Models/Album.swift
@@ -0,0 +1,14 @@
+//
+//  Album.swift
+//  MoodScape
+//
+//
+
+import Foundation
+
+struct Album {
+    let name: String
+    let artist: String
+    let imageUrl: String
+    let spotifyUrl: String
+}
