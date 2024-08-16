c67c30acd0151b9fe92b5935963aaf85d4df2ab8
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-17 01:32:08 +0200

    UIImagePickerController extension added

---
+++ MoodScape/Extensions/ProfileSetup.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSetup.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 17.08.24.
+//
+
+import Foundation
+import UIKit
+
+extension ProfileSetupViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
