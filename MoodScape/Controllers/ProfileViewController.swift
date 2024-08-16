954b24a2f8af4258e5ef7a66fcd0318e8ae5fecd
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-16 17:40:39 +0200

    ProfileView: user's image and edit button added

--- MoodScape/Controllers/ProfileViewController.swift
+++ MoodScape/Controllers/ProfileViewController.swift
@@ -8,19 +8,34 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemPink
-        setupForm()
-        setupConstraints()
-    }
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "defaultProfileImage") // Placeholder image
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.layer.cornerRadius = 75
+        imageView.backgroundColor = .black
+        return imageView
+    }()
+    
+    let editButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit profile", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     let backButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setImage(UIImage(systemName: "arrow.left"), for: .normal)
         button.tintColor = .white
         button.imageView?.contentMode = .scaleAspectFit
-        
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -30,31 +45,52 @@ class ProfileViewController: UIViewController {
         button.setImage(UIImage(systemName: "gear"), for: .normal)
         button.tintColor = .white
         button.imageView?.contentMode = .scaleAspectFit
-        
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    // - MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupForm()
+        setupConstraints()
+    }
     
     // - MARK: SetupForm
     private func setupForm() {
         view.addSubview(backButton)
         view.addSubview(settingsButton)
+        view.addSubview(profileImage)
+        view.addSubview(editButton)
     }
     
     // - MARK: SetupConstraints
     private func setupConstraints() {
         backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
         settingsButton.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
+        editButton.addTarget(self, action: #selector(handleEdit), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             
             settingsButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            settingsButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20)
+            settingsButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
+            
+            profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            profileImage.widthAnchor.constraint(equalToConstant: 150),
+            profileImage.heightAnchor.constraint(equalToConstant: 150),
+            
+            editButton.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20),
+            editButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            editButton.widthAnchor.constraint(equalToConstant: 160),
+            editButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
+    // - MARK: HandleBack
     @objc private func handleBack() {
         dismiss(animated: true, completion: nil)
     }
@@ -65,6 +101,11 @@ class ProfileViewController: UIViewController {
         settingsView.modalTransitionStyle = .crossDissolve
         settingsView.modalPresentationStyle = .fullScreen
         self.present(settingsView, animated: true, completion: nil)
+    }
+    
+    // - MARK: HandleEdit
+    @objc private func handleEdit() {
+        
     }
     
 }
