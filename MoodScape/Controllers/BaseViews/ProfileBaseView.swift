a13297bf5e6d5db3ab8c5e3626a641e27bc498c2
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-17 17:30:53 +0200

    ProfileBaseView added

---
+++ MoodScape/Controllers/BaseViews/ProfileBaseView.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileBaseView.swift
+//  MoodScape
+//
+//
+
+import UIKit
+import Gifu
+
+class ProfileBaseView: UIViewController {
+    
+    let gifBackground: GIFImageView = {
+        let gifImageView = GIFImageView()
+        gifImageView.animate(withGIFNamed: "gradient_skyline_blinking_stars")
+        gifImageView.contentMode = .scaleAspectFill
+        gifImageView.translatesAutoresizingMaskIntoConstraints = false
+        return gifImageView
+    }()
+    
+    let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // - MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupForm()
+        setupConstraints()
+    }
+    
+    // - MARK: SetupForm
+    private func setupForm() {
+        view.addSubview(gifBackground)
+        view.sendSubviewToBack(gifBackground)
+        view.addSubview(backButton)
+    }
+    
+    // - MARK: SetupConstraints
+    private func setupConstraints() {
+        backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            gifBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gifBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gifBackground.topAnchor.constraint(equalTo: view.topAnchor),
+            gifBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                    
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15)
+        ])
+    }
+    
+    // - MARK: HandleBack
+    @objc private func handleBack() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
