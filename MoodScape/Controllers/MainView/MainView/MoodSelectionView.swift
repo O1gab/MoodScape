54bb40e79a59a6cadb461c851de72ee2967c8365
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-19 00:36:31 +0200

    MoodSelectionView changed, emotions selection added

--- MoodScape/Controllers/MainView/MainView/MoodSelectionView.swift
+++ MoodScape/Controllers/MainView/MainView/MoodSelectionView.swift
@@ -6,17 +6,47 @@
 
 import UIKit
 
-class MoodSelectionView: UIViewController {
+class MoodSelectionView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    let contentView: UIView = {
+    private let contentView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 25/255.0, green: 25/255.0, blue: 25/255.0, alpha: 1.0)
         view.layer.cornerRadius = 37
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let closeButton: UIButton = {
+    private let emotions = [
+        "😀", "😢", "😠", "😱", "😴", "😎", "🤔", "😜", "🤯", "😇",
+        "🥳", "🤢", "😭", "😡", "🤬", "🥵", "🥶", "😐", "🙄", "🤤"
+    ]
+        
+    private let emotionsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(EmotionCell.self, forCellWithReuseIdentifier: "EmotionCell")
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+        
+    private let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Save", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+        
+    private var selectedEmotions: [String] = []
+    
+    private let closeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "xmark"), for: .normal)
         button.tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
@@ -27,10 +57,11 @@ class MoodSelectionView: UIViewController {
     // - MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        view.addSubview(contentView)
-        contentView.addSubview(closeButton)
+        setupView()
         setupConstraints()
+        
+        emotionsCollectionView.delegate = self
+        emotionsCollectionView.dataSource = self
     }
     
     // - MARK: ViewWillAppear
@@ -39,40 +70,97 @@ class MoodSelectionView: UIViewController {
         animateShow()
     }
     
+    // - MARK: SetupView
+    private func setupView() {
+        view.addSubview(contentView)
+        contentView.addSubview(emotionsCollectionView)
+        contentView.addSubview(saveButton)
+        contentView.addSubview(closeButton)
+    }
+    
     // - MARK: SetupConstraints
-    func setupConstraints() {
+    private func setupConstraints() {
         closeButton.addTarget(self, action: #selector(closePopUp), for: .touchUpInside)
-        
+        saveButton.addTarget(self, action: #selector(saveEmotions), for: .touchUpInside)
+                
         NSLayoutConstraint.activate([
             contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            contentView.heightAnchor.constraint(equalToConstant: 720),
-            
+            contentView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+                
             closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            closeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20)
+            closeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+                    
+            emotionsCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            emotionsCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            emotionsCollectionView.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 20),
+            emotionsCollectionView.bottomAnchor.constraint(equalTo: saveButton.topAnchor, constant: -20),
+                    
+            saveButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            saveButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            saveButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -40),
+            saveButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
     // - MARK: ClosePopUp
-    @objc func closePopUp() {
-            animateHide()
-        }
+    @objc private func closePopUp() {
+        animateHide()
+    }
+    
+    @objc private func saveEmotions() {
+        // TODO: Save selected emotions and generate a playlist based on the selection
+        dismiss(animated: true, completion: nil)
+    }
     
     // - MARK: AnimateShow
-    func animateShow() {
-           contentView.transform = CGAffineTransform(translationX: 0, y: view.bounds.height)
-           UIView.animate(withDuration: 0.3, animations: {
-               self.contentView.transform = .identity
-           })
-       }
+    private func animateShow() {
+        contentView.transform = CGAffineTransform(translationX: 0, y: view.bounds.height)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.contentView.transform = .identity
+        })
+    }
     
     // - MARK: AnimateHide
-    func animateHide() {
+    private func animateHide() {
         UIView.animate(withDuration: 0.3, animations: {
             self.contentView.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.height)
         }) { _ in
             self.dismiss(animated: false, completion: nil)
         }
+    }
+    
+    // - MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emotions.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmotionCell", for: indexPath) as! EmotionCell
+        cell.emotionLabel.text = emotions[indexPath.item]
+        cell.layer.borderColor = UIColor.white.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 30
+        return cell
+    }
+    
+    // - MARK: UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let padding: CGFloat = 20
+            let availableWidth = collectionView.frame.width - (padding * 2)
+            let itemWidth = (availableWidth / 3) - 10 // Adjust number of items per row as needed
+            return CGSize(width: itemWidth, height: itemWidth)
+        }
+        
+    // - MARK: UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let emotion = emotions[indexPath.item]
+        if selectedEmotions.contains(emotion) {
+            selectedEmotions.removeAll { $0 == emotion }
+        } else {
+            selectedEmotions.append(emotion)
+        }
+        collectionView.reloadItems(at: [indexPath])
     }
 }
