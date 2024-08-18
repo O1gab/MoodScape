db79e949b985f86120f6f70959902f1e4c4a16df
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-18 13:27:59 +0200

    MainTabBar: tab bar changed, main button added

--- MoodScape/Controllers/BaseViews/MainTabBarController.swift
+++ MoodScape/Controllers/BaseViews/MainTabBarController.swift
@@ -10,21 +10,38 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     private let firstViewController = FeedViewController()
     private let secondViewController = MainViewController()
-    private let thirdViewController = StatsViewController()
-    private let fourthViewController = SocialViewController()
+    private let thirdViewController = SocialViewController()
 
+    let mainButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let image = UIImage(systemName: "plus.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .medium))
+        button.setImage(image, for: .normal)
+        button.tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.layer.cornerRadius = 25 // Half of the button's height
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.delegate = self
-
-        self.viewControllers = [firstViewController, secondViewController, thirdViewController, fourthViewController]
-        
+        self.viewControllers = [firstViewController, secondViewController, thirdViewController]
+        setupForm()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateMainButtonPosition()
+    }
+    
+    // - MARK: SetupForm
+    private func setupForm() {
         firstViewController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), tag: 0)
-        secondViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "plus.circle.fill"), tag: 1)
-        thirdViewController.tabBarItem = UITabBarItem(title: "Stats", image: UIImage(systemName: "chart.bar"), tag: 2)
-        fourthViewController.tabBarItem = UITabBarItem(title: "Social", image: UIImage(systemName: "person.3"), tag: 3)
+        thirdViewController.tabBarItem = UITabBarItem(title: "Social", image: UIImage(systemName: "person.3"), tag: 2)
         UITabBar.appearance().tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        
+        mainButton.center = CGPoint(x: tabBar.center.x, y: tabBar.frame.origin.y - 16)
+        mainButton.addTarget(self, action: #selector(mainButtonTapped), for: .touchUpInside)
+        view.addSubview(mainButton)
         
         // - MARK: SWIPE GESTURES
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
@@ -35,6 +52,21 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
         view.addGestureRecognizer(swipeRight)
     }
     
+    // - MARK: UpdateMainButtonPosition
+    func updateMainButtonPosition() {
+        let buttonSize: CGFloat = 50
+        mainButton.frame.size = CGSize(width: buttonSize, height: buttonSize)
+        mainButton.center = CGPoint(
+            x: tabBar.frame.size.width / 2,
+            y: tabBar.frame.size.height + 700
+        )
+    }
+    
+    @objc func mainButtonTapped() {
+        selectedIndex = 1
+    }
+    
+    // - MARK: HandleSwipe
     @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
         if let currentIndex = viewControllers?.firstIndex(of: selectedViewController!) {
             if gesture.direction == .left {
