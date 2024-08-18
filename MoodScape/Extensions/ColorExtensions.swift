9940a84d940b2f6ae18724b1c14f8748979f1995
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-18 16:42:03 +0200

    ColorExtensions: function for contrasting complementary colors added

--- MoodScape/Extensions/ColorExtensions.swift
+++ MoodScape/Extensions/ColorExtensions.swift
@@ -29,9 +29,65 @@ extension UIColor {
         var alpha: CGFloat = 0.0
             
         getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
-            
+        
         let complementaryHue = (hue + 0.5).truncatingRemainder(dividingBy: 1.0)
             
         return UIColor(hue: complementaryHue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+        
+    // Function to calculate brightness of the color
+    private func brightness() -> CGFloat {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+            
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            
+        // Using the formula for luminance to determine brightness
+        return (0.299 * red + 0.587 * green + 0.114 * blue)
+    }
+        
+    func contrastingComplementaryColor() -> UIColor {
+        let compColor = complementaryColor()
+        let compColorBrightness = compColor.brightness()
+           
+        // Ensure the contrasting color is visible on the background
+        if compColorBrightness > 0.5 {
+        // If the complementary color is light, darken it
+            return compColor.darker()
+        } else {
+        // If the complementary color is dark, lighten it
+            return compColor.lighter()
+        }
+    }
+       
+    // Function to lighten a color
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor {
+        return adjustBrightness(by: abs(percentage))
+    }
+       
+    // Function to darken a color
+    func darker(by percentage: CGFloat = 30.0) -> UIColor {
+        return adjustBrightness(by: -abs(percentage))
+    }
+       
+    // Function to adjust brightness
+    private func adjustBrightness(by percentage: CGFloat = 30.0) -> UIColor {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+           
+        if getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(
+                red: min(red + percentage/100, 1.0),
+                   green: min(green + percentage/100, 1.0),
+                   blue: min(blue + percentage/100, 1.0),
+                   alpha: alpha
+            )
+        } else {
+            return self
+        }
     }
 }
