4b97a6d350369b7adfb6b411a6a728d33f18080e
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-18 23:54:20 +0200

    Circles Background: CoreMotion added, appearance changed

--- MoodScape/Controllers/MainView/MainView/CirclesBackgroundView.swift
+++ MoodScape/Controllers/MainView/MainView/CirclesBackgroundView.swift
@@ -5,28 +5,33 @@
 //
 
 import UIKit
+import CoreMotion
 
 class CirclesBackgroundView: UIView {
     
     private var circles: [UIView] = []
     private let circleCount = 5
-    private let circleSizes: [CGFloat] = [50, 70, 90, 110, 130]
+    private let circleSizes: [CGFloat] = [100, 140, 180, 220, 250]
     private let circleColors: [UIColor] = [
-        UIColor.green.withAlphaComponent(0.1),
-        UIColor.green.withAlphaComponent(0.2),
-        UIColor.green.withAlphaComponent(0.3),
-        UIColor.green.withAlphaComponent(0.4),
-        UIColor.green.withAlphaComponent(0.5)
+        UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.1),
+        UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.2),
+        UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.5),
+        UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.4),
+        UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.3)
     ]
+    private let motionManager = CMMotionManager()
+    private let movementFactor: CGFloat = 50
         
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupCircles()
+        startAccelerometerUpdates()
     }
         
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupCircles()
+        startAccelerometerUpdates()
     }
     
     // - MARK: SetupCircles
@@ -82,6 +87,38 @@ class CirclesBackgroundView: UIView {
                 animations: {
                 circle.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
             }, completion: nil)
+        }
+    }
+    
+    // - MARK: StartAccelerometerUpdates
+    private func startAccelerometerUpdates() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.1 // Update interval
+            motionManager.startAccelerometerUpdates(to: .main) { [weak self] data, error in
+            guard let data = data, error == nil else { return }
+                self?.updateCirclesPosition(with: data.acceleration)
+            }
+        }
+    }
+        
+    // - MARK: UpdateCirclesPosition
+    private func updateCirclesPosition(with acceleration: CMAcceleration) {
+        let shiftX = CGFloat(acceleration.x) * movementFactor
+        let buttonCenterX = bounds.midX
+        let buttonCenterY = bounds.midY
+            
+        for (index, circle) in circles.enumerated() {
+            let size = circleSizes[index]
+            let offsetX = circleSizes[index] / 2 + shiftX
+            let offsetY = circleSizes[index] / 2
+                
+            circle.frame = CGRect(
+                x: buttonCenterX + offsetX - size / 2,
+                y: buttonCenterY + offsetY - size / 2,
+                width: size,
+                height: size
+            )
+            circle.layer.cornerRadius = size / 2
         }
     }
 }
