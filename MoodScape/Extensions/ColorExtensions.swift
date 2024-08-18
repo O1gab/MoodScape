7490f14cfa07f791bd3844c9150d2c4fe1f5259f
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-18 15:16:45 +0200

    Extension for UIColor (contrasting color) added

---
+++ MoodScape/Extensions/ColorExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  ColorExtensions.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 18.08.24.
+//
+
+import UIKit
+
+extension UIColor {
+    // Calculates the luminance to determine if the contrasting color should be black or white
+    func contrastingColor() -> UIColor {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        let luminance = (0.299 * red + 0.587 * green + 0.114 * blue)
+        
+        return luminance > 0.5 ? UIColor.black : UIColor.white
+    }
+}
