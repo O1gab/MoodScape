219a4cc8bc255c6bd2ec3ce6dcaf2d3d4ca8979e
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-19 02:47:36 +0200

    SettingsView: exit (logout) function added

--- MoodScape/Controllers/SettingsView/SettingsViewController.swift
+++ MoodScape/Controllers/SettingsView/SettingsViewController.swift
@@ -5,10 +5,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SettingsViewController: ProfileBaseView {
     
-    let settingsLabel: UILabel = {
+    private let settingsLabel: UILabel = {
         let settingsLabel = UILabel()
         settingsLabel.text = "Settings"
         settingsLabel.textColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
@@ -17,21 +18,45 @@ class SettingsViewController: ProfileBaseView {
         return settingsLabel
     }()
    
+    private let exitButton: UIButton = {
+        let exitButton = UIButton(type: .system)
+        exitButton.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
+        exitButton.tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        return exitButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupForm()
+        setupView()
         setupConstraints()
     }
     
-    private func setupForm() {
+    private func setupView() {
         view.addSubview(settingsLabel)
+        view.addSubview(exitButton)
+        
+        exitButton.addTarget(self, action: #selector(exitTapped), for: .touchUpInside)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             settingsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            settingsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            settingsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            exitButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            exitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
         ])
     }
-
+    
+    // - MARK: ExitTapped
+    @objc private func exitTapped() {
+        do {
+            try Auth.auth().signOut()
+            let startView = StartViewController()
+            startView.modalPresentationStyle = .fullScreen
+            self.present(startView, animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
