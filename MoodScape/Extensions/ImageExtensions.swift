b25b33c8a06363abf0abc1f67c38838845247abb
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-20 21:06:50 +0200

    UIImage extension for gradient added

--- MoodScape/Extensions/ImageExtensions.swift
+++ MoodScape/Extensions/ImageExtensions.swift
@@ -28,7 +28,45 @@ extension UIImage {
         let g = CGFloat(buffer[1]) / 255.0
         let b = CGFloat(buffer[2]) / 255.0
         let a = CGFloat(buffer[3]) / 255.0
-        
+            
         return UIColor(red: r, green: g, blue: b, alpha: a)
     }
+    
+    func applyCircularGradientOverlay(colors: [UIColor]) -> UIImage? {
+            let size = self.size
+            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+            guard let context = UIGraphicsGetCurrentContext() else { return nil }
+
+            // Draw the original image
+            self.draw(in: CGRect(origin: .zero, size: size))
+
+            // Create a circular gradient
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = CGRect(origin: .zero, size: size)
+            gradientLayer.colors = colors.map { $0.cgColor }
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+
+            // Create a circular path
+            let radius = min(size.width, size.height) / 2
+            let center = CGPoint(x: size.width / 2, y: size.height / 2)
+            let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+            
+            // Create a shape layer and apply the circular path
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = circlePath.cgPath
+            shapeLayer.fillColor = UIColor.clear.cgColor
+            shapeLayer.mask = gradientLayer
+            
+            // Add the shape layer to the context
+            context.saveGState()
+            shapeLayer.render(in: context)
+            context.restoreGState()
+            
+            // Get the image with gradient
+            let gradientImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            return gradientImage
+        }
 }
