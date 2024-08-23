38b4a4b8599856551e0c6cddd600678ca11f411e
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-23 16:54:16 +0200

    SpotifySetupView: configured

--- MoodScape/Controllers/SetupView/SpotifySetupViewController.swift
+++ MoodScape/Controllers/SetupView/SpotifySetupViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class SpotifySetupView: SetupBaseView {
     
+    private let fieldLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let spotifyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Connect to Spotify", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        button.setTitleColor(UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.9), for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     // - MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +39,34 @@ class SpotifySetupView: SetupBaseView {
     // - MARK: ViewDidAppear
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Please, connect your account to Spotify", typingSpeed: self?.typingSpeed ?? 0.075) {}
+        }
     }
     
     // - MARK: SetupView
     private func setupView() {
-        
+        view.addSubview(fieldLabel)
+        view.addSubview(spotifyButton)
     }
     
     // - MARK: SetupConstraints
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350),
+            fieldLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15),
+            
+            spotifyButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 390),
+            spotifyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spotifyButton.widthAnchor.constraint(equalToConstant: 210),
+            spotifyButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
+    @objc private func connectSpotify() {
+        // TODO: connect to the user's account on Spotify
+    }
 
 }
