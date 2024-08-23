37849c1d1af409cfa4ef17d4d1c5e57d4ddf5561
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-23 10:44:28 +0200

    UserSetupView: submit and skip buttons added, typing and erasing animation added

--- MoodScape/Controllers/SetupView/UserSetupViewController.swift
+++ MoodScape/Controllers/SetupView/UserSetupViewController.swift
@@ -22,7 +22,6 @@ class UserSetupView: UIViewController {
     
     private let fieldLabel: UILabel = {
         let label = UILabel()
-        label.text = "Enter your first name"
         label.textColor = .white
         label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
         label.textAlignment = .left
@@ -42,6 +41,29 @@ class UserSetupView: UIViewController {
         name.translatesAutoresizingMaskIntoConstraints = false
         return name
     }()
+    
+    private let submitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Submit", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        button.setTitleColor(UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.75), for: .normal)
+        button.backgroundColor = .white.withAlphaComponent(0.5)
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.setTitleColor(UIColor(red: 179/255, green: 23/255, blue: 23/255, alpha: 1.0), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let typingSpeed: TimeInterval = 0.075
+    private var timer: Timer?
 
     // - MARK: ViewDidLoad
     override func viewDidLoad() {
@@ -52,8 +74,17 @@ class UserSetupView: UIViewController {
         setupView()
         setupConstraints()
     }
-  
     
+    // - MARK: ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Enter your first name:", typingSpeed: self?.typingSpeed ?? 0.075) {
+                // TODO:
+            }
+        }
+    }
+  
     // - MARK: SetupView
     private func setupView() {
         view.addSubview(gifBackground)
@@ -61,20 +92,74 @@ class UserSetupView: UIViewController {
         
         view.addSubview(fieldLabel)
         view.addSubview(textField)
+        view.addSubview(submitButton)
+        view.addSubview(skipButton)
     }
     
     // - MARK: SetupConstraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 220),
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 250),
             fieldLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15),
             
             textField.topAnchor.constraint(equalTo: fieldLabel.bottomAnchor, constant: 20),
             textField.leftAnchor.constraint(equalTo: fieldLabel.leftAnchor),
-            textField.widthAnchor.constraint(equalToConstant: 250),
-            textField.heightAnchor.constraint(equalToConstant: 55)
+            textField.widthAnchor.constraint(equalToConstant: 300),
+            textField.heightAnchor.constraint(equalToConstant: 60),
+            
+            skipButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30),
+            skipButton.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
+            skipButton.widthAnchor.constraint(equalToConstant: 120),
+            skipButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            submitButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30),
+            submitButton.leadingAnchor.constraint(equalTo: skipButton.trailingAnchor, constant: 45),
+            submitButton.widthAnchor.constraint(equalToConstant: 120),
+            submitButton.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    // - MARK: StartTypingAnimation
+    private func startTypingAnimation(label: UILabel, text: String, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
+        let fullText = text
+        label.text = ""
+        
+        var index = 0
+        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            
+            if index <= fullText.count {
+                let endIndex = fullText.index(fullText.startIndex, offsetBy: index)
+                label.text = String(fullText[..<endIndex])
+                index += 1
+            } else {
+                timer.invalidate()
+                completion()
+            }
+        }
+    }
+    
+    // - MARK: StartErasingAnimation
+    private func startErasingAnimation(label: UILabel, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
+        guard let text = label.text, !text.isEmpty else {
+            completion()
+            return
+        }
+        
+        var index = text.count
+        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            
+            if index > 0 {
+                let endIndex = text.index(text.startIndex, offsetBy: index - 1)
+                label.text = String(text[..<endIndex])
+                index -= 1
+            } else {
+                timer.invalidate()
+                completion()
+            }
+        }
     }
     
     // - MARK: SetupGradientBackground
