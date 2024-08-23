ab1608da5008a5a609780a2dc4ff1d5e573918b3
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-23 16:31:12 +0200

    SetupBaseView added and integrated into the classes

--- MoodScape/Controllers/SetupView/StartSetupViewController.swift
+++ MoodScape/Controllers/SetupView/StartSetupViewController.swift
@@ -8,18 +8,8 @@
 import UIKit
 import Gifu
 
-class StartSetupView: UIViewController {
+class StartSetupView: SetupBaseView {
 
-    private let gradientLayer = CAGradientLayer()
-    
-    private let gifBackground: GIFImageView = {
-        let gifBackground = GIFImageView()
-        gifBackground.animate(withGIFNamed: "gradient_skyline_blinking_stars")
-        gifBackground.contentMode = .scaleAspectFill
-        gifBackground.translatesAutoresizingMaskIntoConstraints = false
-        return gifBackground
-    }()
-    
     private let messageLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
@@ -40,15 +30,9 @@ class StartSetupView: UIViewController {
         return label
     }()
     
-    private let typingSpeed: TimeInterval = 0.075
-    private var timer: Timer?
-    
     // - MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.backgroundColor = UIColor(red: 35/255.0, green: 35/255.0, blue: 35/255.0, alpha: 1.0)
-        setupGradientBackground()
-        animateGradient()
         setupView()
         setupConstraints()
     }
@@ -81,9 +65,6 @@ class StartSetupView: UIViewController {
 
     // - MARK: SetupView
     private func setupView() {
-        view.addSubview(gifBackground)
-        view.sendSubviewToBack(gifBackground)
-        
         view.addSubview(messageLabel)
         view.addSubview(appLabel)
     }
@@ -101,48 +82,6 @@ class StartSetupView: UIViewController {
         ])
     }
 
-    // - MARK: StartTypingAnimation
-    private func startTypingAnimation(label: UILabel, text: String, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
-        let fullText = text
-        label.text = ""
-        
-        var index = 0
-        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
-            guard let self = self else { return }
-            
-            if index <= fullText.count {
-                let endIndex = fullText.index(fullText.startIndex, offsetBy: index)
-                label.text = String(fullText[..<endIndex])
-                index += 1
-            } else {
-                timer.invalidate()
-                completion()
-            }
-        }
-    }
-    
-    // - MARK: StartErasingAnimation
-    private func startErasingAnimation(label: UILabel, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
-        guard let text = label.text, !text.isEmpty else {
-            completion()
-            return
-        }
-        
-        var index = text.count
-        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
-            guard let self = self else { return }
-            
-            if index > 0 {
-                let endIndex = text.index(text.startIndex, offsetBy: index - 1)
-                label.text = String(text[..<endIndex])
-                index -= 1
-            } else {
-                timer.invalidate()
-                completion()
-            }
-        }
-    }
-
     // - MARK: TransitionToNextView
     private func transitionToNextView() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
@@ -157,36 +96,5 @@ class StartSetupView: UIViewController {
             self.view.window?.layer.add(transition, forKey: kCATransition)
             self.present(nextViewController, animated: false, completion: nil)
         }
-    }
-    
-    // - MARK: SetupGradientBackground
-    private func setupGradientBackground() {
-        let baseColor = UIColor(red: 35/255.0, green: 35/255.0, blue: 35/255.0, alpha: 0.5).cgColor
-        let darkColor = UIColor(red: 25/255.0, green: 25/255.0, blue: 25/255.0, alpha: 0.5).cgColor
-        let lightColor = UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor
-        
-        gradientLayer.colors = [darkColor, baseColor, lightColor]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        gradientLayer.frame = view.bounds
-        
-        view.layer.insertSublayer(gradientLayer, at: 0)
-    }
-    
-    // - MARK: AnimateGradient
-    private func animateGradient() {
-        let animation = CABasicAnimation(keyPath: "colors")
-        animation.fromValue = [UIColor(red: 70/255.0, green: 70/255.0, blue: 70/255.0, alpha: 0.35).cgColor,
-                               UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor,
-                               UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor]
-        animation.toValue = [UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 0.5).cgColor,
-                             UIColor(red: 25/255.0, green: 35/255.0, blue: 25/255.0, alpha: 0.5).cgColor,
-                             UIColor(red: 0.0, green: 0.4, blue: 0.31, alpha: 1.0).cgColor]
-        animation.duration = 7.0
-        animation.autoreverses = true
-        animation.repeatCount = .infinity
-        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        
-        gradientLayer.add(animation, forKey: "gradientAnimation")
     }
 }
--- MoodScape/Controllers/SetupView/UserSetupViewController.swift
+++ MoodScape/Controllers/SetupView/UserSetupViewController.swift
@@ -11,7 +11,7 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 
-class UserSetupView: UIViewController {
+class UserSetupView: SetupBaseView {
 
     private let gradientLayer = CAGradientLayer()
     private var currentQuestionIndex = 0
@@ -68,16 +68,10 @@ class UserSetupView: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
-    private let typingSpeed: TimeInterval = 0.075
-    private var timer: Timer?
 
     // - MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.backgroundColor = UIColor(red: 35/255.0, green: 35/255.0, blue: 35/255.0, alpha: 1.0)
-        setupGradientBackground()
-        animateGradient()
         setupView()
         setupConstraints()
     }
@@ -92,9 +86,6 @@ class UserSetupView: UIViewController {
   
     // - MARK: SetupView
     private func setupView() {
-        view.addSubview(gifBackground)
-        view.sendSubviewToBack(gifBackground)
-        
         view.addSubview(fieldLabel)
         view.addSubview(textField)
         view.addSubview(submitButton)
@@ -126,79 +117,6 @@ class UserSetupView: UIViewController {
             submitButton.widthAnchor.constraint(equalToConstant: 120),
             submitButton.heightAnchor.constraint(equalToConstant: 50)
         ])
-    }
-    
-    // - MARK: StartTypingAnimation
-    private func startTypingAnimation(label: UILabel, text: String, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
-        let fullText = text
-        label.text = ""
-        
-        var index = 0
-        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
-            guard let self = self else { return }
-            
-            if index <= fullText.count {
-                let endIndex = fullText.index(fullText.startIndex, offsetBy: index)
-                label.text = String(fullText[..<endIndex])
-                index += 1
-            } else {
-                timer.invalidate()
-                completion()
-            }
-        }
-    }
-    
-    // - MARK: StartErasingAnimation
-    private func startErasingAnimation(label: UILabel, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
-        guard let text = label.text, !text.isEmpty else {
-            completion()
-            return
-        }
-        
-        var index = text.count
-        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
-            guard let self = self else { return }
-            
-            if index > 0 {
-                let endIndex = text.index(text.startIndex, offsetBy: index - 1)
-                label.text = String(text[..<endIndex])
-                index -= 1
-            } else {
-                timer.invalidate()
-                completion()
-            }
-        }
-    }
-    
-    // - MARK: SetupGradientBackground
-    private func setupGradientBackground() {
-        let baseColor = UIColor(red: 35/255.0, green: 35/255.0, blue: 35/255.0, alpha: 0.5).cgColor
-        let darkColor = UIColor(red: 25/255.0, green: 25/255.0, blue: 25/255.0, alpha: 0.5).cgColor
-        let lightColor = UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor
-        
-        gradientLayer.colors = [darkColor, baseColor, lightColor]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        gradientLayer.frame = view.bounds
-        
-        view.layer.insertSublayer(gradientLayer, at: 0)
-    }
-    
-    // - MARK: AnimateGradient
-    private func animateGradient() {
-        let animation = CABasicAnimation(keyPath: "colors")
-        animation.fromValue = [UIColor(red: 70/255.0, green: 70/255.0, blue: 70/255.0, alpha: 0.35).cgColor,
-                               UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor,
-                               UIColor(red: 45/255.0, green: 45/255.0, blue: 45/255.0, alpha: 0.5).cgColor]
-        animation.toValue = [UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 0.5).cgColor,
-                             UIColor(red: 25/255.0, green: 35/255.0, blue: 25/255.0, alpha: 0.5).cgColor,
-                             UIColor(red: 0.0, green: 0.4, blue: 0.31, alpha: 1.0).cgColor]
-        animation.duration = 7.0
-        animation.autoreverses = true
-        animation.repeatCount = .infinity
-        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        
-        gradientLayer.add(animation, forKey: "gradientAnimation")
     }
     
     // - MARK: HandleSubmit
@@ -252,7 +170,7 @@ class UserSetupView: UIViewController {
     // - MARK: NavigateToNextView
     private func navigateToNextView() {
         // TODO: implement next view
-        let nextViewController = FeedViewController()
+        let nextViewController = ImageSetupView()
         nextViewController.modalPresentationStyle = .fullScreen
         let transition = CATransition()
         transition.duration = 0.5
