cc2d43c0de8cb3d2c91f94e3241b11306743d987
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-23 23:47:51 +0200

    MusicSetupView: base implemented

---
+++ MoodScape/Controllers/SetupView/MusicSetupViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MusicSetupController.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 23.08.24.
+//
+
+import UIKit
+
+class MusicSetupView: SetupBaseView {
+    
+    private let fieldLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.numberOfLines = 2
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let submitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Submit", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        button.setTitleColor(UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.9), for: .normal)
+        button.backgroundColor = .white.withAlphaComponent(0.5)
+        button.layer.cornerRadius = 25
+        button.alpha = 0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // - MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+    }
+    
+    // - MARK: ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Choose the music genres you listen to the most", typingSpeed: 0.05) {
+                // reveal selection view
+            }
+        }
+    }
+    
+    // - MARK: SetupView
+    private func setupView() {
+        view.addSubview(fieldLabel)
+        view.addSubview(submitButton)
+    }
+    
+    // - MARK: SetupConstraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 270),
+            fieldLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15),
+            
+            submitButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350),
+            submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            submitButton.widthAnchor.constraint(equalToConstant: 210),
+            submitButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
