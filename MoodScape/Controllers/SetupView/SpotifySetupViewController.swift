3085305ffaae05cff67f668158485a199ef02118
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-23 23:29:06 +0200

    SpotifySetupView: buttons and animation added

--- MoodScape/Controllers/SetupView/SpotifySetupViewController.swift
+++ MoodScape/Controllers/SetupView/SpotifySetupViewController.swift
@@ -13,6 +13,7 @@ class SpotifySetupView: SetupBaseView {
         let label = UILabel()
         label.textColor = .white
         label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.numberOfLines = 2
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -25,6 +26,17 @@ class SpotifySetupView: SetupBaseView {
         button.setTitleColor(UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.9), for: .normal)
         button.backgroundColor = .white
         button.layer.cornerRadius = 25
+        button.alpha = 0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.setTitleColor(UIColor(red: 181/255, green: 23/255, blue: 23/255, alpha: 1.0), for: .normal)
+        button.alpha = 0
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -40,7 +52,10 @@ class SpotifySetupView: SetupBaseView {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Please, connect your account to Spotify", typingSpeed: self?.typingSpeed ?? 0.075) {}
+            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Please, connect your account to Spotify", typingSpeed: 0.05) {
+                self?.revealButton(button: self?.spotifyButton ?? UIButton())
+                self?.revealButton(button: self?.skipButton ?? UIButton())
+            }
         }
     }
     
@@ -48,25 +63,49 @@ class SpotifySetupView: SetupBaseView {
     private func setupView() {
         view.addSubview(fieldLabel)
         view.addSubview(spotifyButton)
+        view.addSubview(skipButton)
     }
     
     // - MARK: SetupConstraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             
-            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350),
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 270),
             fieldLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15),
             
-            spotifyButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 390),
+            spotifyButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350),
             spotifyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             spotifyButton.widthAnchor.constraint(equalToConstant: 210),
-            spotifyButton.heightAnchor.constraint(equalToConstant: 50)
+            spotifyButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            skipButton.topAnchor.constraint(equalTo: spotifyButton.bottomAnchor, constant: 20),
+            skipButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            skipButton.widthAnchor.constraint(equalToConstant: 120),
+            skipButton.heightAnchor.constraint(equalToConstant: 50),
         ])
+    }
+    
+    // - MARK: NavigateToNextView
+    private func navigateToNextView() {
+        // TODO: implement next view
+        let nextViewController = MainViewController()
+        nextViewController.modalPresentationStyle = .fullScreen
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = .push
+        transition.subtype = .fromRight
+        transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        self.present(nextViewController, animated: false, completion: nil)
     }
     
     @objc private func connectSpotify() {
         // TODO: connect to the user's account on Spotify
     }
-
+    
+    @objc private func handleSkip() {
+        navigateToNextView()
+    }
 }
