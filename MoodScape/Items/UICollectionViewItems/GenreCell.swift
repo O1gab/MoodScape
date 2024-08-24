1070317d40c5159cb229e367b4c38d7689b51d43
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-24 23:44:31 +0200

    Genre model added, GenreCell for UICollectionView added

---
+++ MoodScape/Items/UICollectionViewItems/GenreCell.swift
@@ -0,0 +1,45 @@
+//
+//  GenreCell.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 24.08.24.
+//
+
+import UIKit
+
+class GenreCell: UICollectionViewCell {
+    
+    static let identifier = "GenreCell"
+        
+        private let genreLabel: UILabel = {
+            let label = UILabel()
+            label.textColor = .white
+            label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+            label.textAlignment = .center
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            contentView.addSubview(genreLabel)
+            contentView.backgroundColor = .gray
+            contentView.layer.cornerRadius = 15
+            
+            NSLayoutConstraint.activate([
+                genreLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+                genreLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+                genreLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+                genreLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            ])
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func configure(with genre: String, isSelected: Bool) {
+            genreLabel.text = genre
+            contentView.backgroundColor = isSelected ? UIColor.green : UIColor.gray
+        }
+}
