d828cea7e2e5f16aef483b6ce15c459f690bcfdf
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-25 18:04:50 +0200

    EndSetupView implemented

---
+++ MoodScape/Controllers/SetupView/EndSetupViewController.swift
@@ -0,0 +1,72 @@
+//
+//  EndSetupViewController.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 25.08.24.
+//
+
+import UIKit
+import Gifu
+
+class EndSetupView: SetupBaseView {
+    
+    private let gifGradient: GIFImageView = {
+        let gifBackground = GIFImageView()
+        gifBackground.animate(withGIFNamed: "green_gradient")
+        gifBackground.contentMode = .scaleAspectFill
+        gifBackground.alpha = 0.5
+        gifBackground.translatesAutoresizingMaskIntoConstraints = false
+        return gifBackground
+    }()
+    
+    private let fieldLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // - MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+    }
+    
+    // - MARK: ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: "Thank you for configuring your profile! Now we are configuring the app for you.", typingSpeed: self?.typingSpeed ?? 0.05) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self?.startErasingAnimation(label: self?.fieldLabel ?? UILabel(), typingSpeed: 0.035) {
+                        self?.navigateToNextView(viewController: MainTabBarController())
+                    }
+                }
+            }
+        }
+    }
+  
+    // - MARK: SetupView
+    private func setupView() {
+        view.addSubview(gifGradient)
+        view.addSubview(fieldLabel)
+    }
+    
+    // - MARK: SetupConstraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            gifGradient.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gifGradient.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gifGradient.topAnchor.constraint(equalTo: view.topAnchor),
+            gifGradient.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 250),
+            fieldLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15)
+        ])
+    }
+}
