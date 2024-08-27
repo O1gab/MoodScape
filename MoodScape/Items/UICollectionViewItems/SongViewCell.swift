69ec6221170b6939795a51f16560e2809eb4299e
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-27 23:51:56 +0200

    SongViewCell item added

---
+++ MoodScape/Items/UICollectionViewItems/SongViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  SongCard.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 27.08.24.
+//
+
+import UIKit
+
+class SongViewCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with song: Song) {
+        imageView.image = nil // Set image from URL if needed
+        // Optionally load image asynchronously
+    }
+}
