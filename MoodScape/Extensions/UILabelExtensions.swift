6e4689d6fbee6e5d1b76ee4974e0c0c76c302276
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-30 17:58:35 +0200

    UILabelExtensions: start erasing animation function moved to here

--- MoodScape/Extensions/UILabelExtensions.swift
+++ MoodScape/Extensions/UILabelExtensions.swift
@@ -9,6 +9,7 @@ import UIKit
 
 extension UILabel {
     
+    // - MARK: StartTypingAnimation
     func startTypingAnimation(label: UILabel, text: String, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
         var timer: Timer?
         let fullText = text
@@ -22,6 +23,29 @@ extension UILabel {
                 let endIndex = fullText.index(fullText.startIndex, offsetBy: index)
                 label.text = String(fullText[..<endIndex])
                 index += 1
+            } else {
+                timer.invalidate()
+                completion()
+            }
+        }
+    }
+    
+    // - MARK: StartErasingAnimation
+    func startErasingAnimation(label: UILabel, typingSpeed: TimeInterval, completion: @escaping () -> Void) {
+        var timer: Timer?
+        guard let text = label.text, !text.isEmpty else {
+            completion()
+            return
+        }
+        
+        var index = text.count
+        timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            
+            if index > 0 {
+                let endIndex = text.index(text.startIndex, offsetBy: index - 1)
+                label.text = String(text[..<endIndex])
+                index -= 1
             } else {
                 timer.invalidate()
                 completion()
