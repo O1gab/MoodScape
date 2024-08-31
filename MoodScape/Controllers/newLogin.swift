0837ca878db59d16e2d07f99af619edb8d66f292
Olga Batiunia <olgabatiunia@gmail.com>
2024-08-31 17:42:38 +0200

    newLogin view added

---
+++ MoodScape/Controllers/newLogin.swift
@@ -0,0 +1,210 @@
+//
+//  newLogin.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 31.08.24.
+//
+
+import UIKit
+
+class newLogin: StartBaseView {
+    private var currentQuestionIndex = 0
+    private let questions = ["Enter your email or username here:", "Enter your password:"]
+    
+    private let backButton: UIButton = {
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+        backButton.tintColor = .white
+        backButton.imageView?.contentMode = .scaleAspectFit
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        return backButton
+    }()
+    
+    private let fieldLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let textField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .none
+        field.textColor = .white
+        field.layer.borderColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.75).cgColor
+        field.layer.borderWidth = 2
+        field.layer.cornerRadius = 15
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: field.frame.height))
+        field.leftViewMode = .always
+        field.autocapitalizationType = .none
+        field.alpha = 0
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Submit", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        button.setTitleColor(UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 0.9), for: .normal)
+        button.backgroundColor = .white.withAlphaComponent(0.5)
+        button.layer.cornerRadius = 25
+        button.alpha = 0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let notificationMessage: UILabel = {
+        let notificationMessage = UILabel()
+        notificationMessage.textColor = .red
+        notificationMessage.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        notificationMessage.numberOfLines = 0
+        notificationMessage.textAlignment = .left
+        notificationMessage.lineBreakMode = .byWordWrapping
+        notificationMessage.adjustsFontSizeToFitWidth = true
+        notificationMessage.minimumScaleFactor = 0.5
+        notificationMessage.isHidden = true
+        notificationMessage.translatesAutoresizingMaskIntoConstraints = false
+        return notificationMessage
+    }()
+
+    private let eyeButton: UIButton = {
+        let eyeButton = UIButton(type: .custom)
+        eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        eyeButton.tintColor = .white
+        eyeButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        eyeButton.addTarget(newRegistration.self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+        eyeButton.alpha = 0
+        return eyeButton
+    }()
+    
+    // - MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+    }
+    
+    // - MARK: ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.fieldLabel.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: self?.questions[self?.currentQuestionIndex ?? 0] ?? "", typingSpeed: 0.05) {
+                UIView.animate(withDuration: 2.0) {
+                    self?.textField.alpha = 1.0
+                    self?.loginButton.alpha = 1.0
+                }
+            }
+        }
+    }
+    
+    // - MARK: SetupView
+    private func setupView() {
+        view.addSubview(fieldLabel)
+        view.addSubview(textField)
+        view.addSubview(eyeButton)
+        view.addSubview(loginButton)
+        view.addSubview(notificationMessage)
+        view.addSubview(backButton)
+        
+        loginButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
+    }
+    
+    // - MARK: SetupConstraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            
+            fieldLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 250),
+            fieldLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            fieldLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 15),
+            
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 290),
+            textField.leftAnchor.constraint(equalTo: fieldLabel.leftAnchor),
+            textField.widthAnchor.constraint(equalToConstant: 300),
+            textField.heightAnchor.constraint(equalToConstant: 60),
+
+            loginButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30),
+            loginButton.leadingAnchor.constraint(equalTo: textField.leadingAnchor, constant: 10),
+            loginButton.widthAnchor.constraint(equalToConstant: 120),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            notificationMessage.topAnchor.constraint(equalTo: loginButton.topAnchor),
+            notificationMessage.centerYAnchor.constraint(equalTo: loginButton.centerYAnchor),
+            notificationMessage.leadingAnchor.constraint(equalTo: loginButton.trailingAnchor, constant: 5),
+            notificationMessage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            notificationMessage.heightAnchor.constraint(equalToConstant: 350)
+        ])
+    }
+    
+    private func handleLogin() {
+        
+    }
+    
+    // - MARK: HandleSubmit
+    @objc private func handleSubmit() {
+        guard let answer = textField.text, !answer.isEmpty else {
+            showErrorMessage("The field cannot be empty")
+            return
+        }
+        
+        if currentQuestionIndex == 0 {
+            
+            return
+        }
+        else if currentQuestionIndex == 1 {
+           
+            return
+        }
+        proceedToNextQuestion()
+    }
+    
+    // - MARK: ProceedToNextQuestion
+    private func proceedToNextQuestion() {
+        self.fieldLabel.startErasingAnimation(label: fieldLabel, typingSpeed: 0.05) { [weak self] in
+            self?.textField.text = ""
+            
+            if self?.currentQuestionIndex ?? 0 < self?.questions.count ?? 0 {
+                let nextQuestion = self?.questions[self?.currentQuestionIndex ?? 0] ?? ""
+                self?.fieldLabel.startTypingAnimation(label: self?.fieldLabel ?? UILabel(), text: nextQuestion, typingSpeed: 0.05) {}
+            } else {
+                // HANDLE LOGIN
+                //self?.handleLoign()
+            }
+        }
+    }
+    
+    // - MARK: ShowErrorMessage
+    private func showErrorMessage(_ message: String) {
+        notificationMessage.text = message
+        notificationMessage.textColor = .red
+        notificationMessage.isHidden = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) { [weak self] in
+            self?.notificationMessage.startErasingAnimation(label: self?.notificationMessage ?? UILabel(), typingSpeed: 0.05) {}
+        }
+    }
+    
+    // - MARK: ShowSuccessMessage
+    private func showSuccessMessage(_ message: String) {
+        notificationMessage.text = message
+        notificationMessage.textColor = .green
+        notificationMessage.isHidden = false
+    }
+    
+    // - MARK: TogglePasswordVisibility
+    @objc func togglePasswordVisibility() {
+        textField.isSecureTextEntry.toggle()
+        let imageName = textField.isSecureTextEntry ? "eye.slash" : "eye"
+        textField.eyeButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    
+    // - MARK: HandleBack
+    @objc private func handleBack() {
+        dismiss(animated: true, completion: nil)
+    }
+}
