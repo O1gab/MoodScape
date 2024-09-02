3c1c219af1aa426fcd541c091ab9d114ef70f943
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-02 21:45:52 +0200

    PreferencesCell for ProfileView implemented

---
+++ MoodScape/Items/UICollectionViewItems/PreferencesCell.swift
@@ -0,0 +1,51 @@
+//
+//  PreferencesCell.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 02.09.24.
+//
+
+import UIKit
+
+class PreferencesCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: - Setup View
+    private func setupView() {
+        contentView.addSubview(imageView)
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Configure Cell
+    func configure(with image: UIImage?) {
+        imageView.image = image
+    }
+}
