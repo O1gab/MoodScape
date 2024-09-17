751a8dd0b8c3b66aad926b7192e0b9d1f776c349
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-17 19:37:20 +0200

    NewPlaylistView: new properties added, view set up

--- MoodScape/Controllers/MainView/MainView/NewPlaylistView.swift
+++ MoodScape/Controllers/MainView/MainView/NewPlaylistView.swift
@@ -7,62 +7,114 @@
 
 import UIKit
 
-class NewPlaylistView: UIView {
+class NewPlaylistView: UIViewController {
     
-    private let imageView = UIImageView()
+    private let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray.withAlphaComponent(0.9)
+        view.layer.cornerRadius = 25
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .darkGray
+        view.layer.cornerRadius = 25
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let playlistDate: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
+    }()
 
     // Confetti animation view
-    //private let confettiView = ConfettiView()
+    private let confettiView = ConfettiView()
 
-    
     private let openSpotifyButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Go to Spotify", for: .normal)
+        button.setTitle("Open on Spotify", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 10
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.layer.cornerRadius = 25
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private let spotifyLogo: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Spotify_Logo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(weight: .bold)), for: .normal)
+        button.tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
         setupView()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    // MARK: ViewWillAppear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateShow()
     }
     
+    // MARK: - SetupView
     private func setupView() {
-        backgroundColor = .black.withAlphaComponent(0.8)
+        view.addSubview(contentView)
+        view.addSubview(confettiView)
+        contentView.addSubview(closeButton)
+        contentView.addSubview(imageView)
+        contentView.addSubview(openSpotifyButton)
+        contentView.addSubview(spotifyLogo)
         
-        addSubview(imageView)
-        addSubview(openSpotifyButton)
-        //addSubview(confettiView)
         
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        //confettiView.translatesAutoresizingMaskIntoConstraints = false
+        confettiView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
-            imageView.widthAnchor.constraint(equalToConstant: 150),
-            imageView.heightAnchor.constraint(equalToConstant: 150),
+            contentView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            contentView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            contentView.widthAnchor.constraint(equalToConstant: 350),
+            contentView.heightAnchor.constraint(equalToConstant: 550),
             
+            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            closeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             
-            openSpotifyButton.centerXAnchor.constraint(equalTo: centerXAnchor),
-            openSpotifyButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 100),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 250),
+            imageView.heightAnchor.constraint(equalToConstant: 250),
+            
+            openSpotifyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            openSpotifyButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 100),
             openSpotifyButton.widthAnchor.constraint(equalToConstant: 200),
-            openSpotifyButton.heightAnchor.constraint(equalToConstant: 50)
-            /*
-            confettiView.topAnchor.constraint(equalTo: topAnchor),
-            confettiView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            confettiView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            confettiView.bottomAnchor.constraint(equalTo: bottomAnchor),
-             */
+            openSpotifyButton.heightAnchor.constraint(equalToConstant: 55),
+            
+            spotifyLogo.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5),
+            spotifyLogo.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            spotifyLogo.heightAnchor.constraint(equalToConstant: 50),
+            spotifyLogo.widthAnchor.constraint(equalToConstant: 100),
+            
+            confettiView.topAnchor.constraint(equalTo: view.topAnchor),
+            confettiView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            confettiView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            confettiView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+             
         ])
-        
+        closeButton.addTarget(self, action: #selector(closePopUp), for: .touchUpInside)
         openSpotifyButton.addTarget(self, action: #selector(openSpotifyTapped), for: .touchUpInside)
     }
 
@@ -75,6 +127,29 @@ class NewPlaylistView: UIView {
         imageView.backgroundColor = color
         
         //confettiView.startConfettiAnimation()
+    }
+    
+    // MARK: - ClosePopUp
+    @objc private func closePopUp() {
+        animateHide()
+    }
+    
+    // MARK: - AnimateShow
+    private func animateShow() {
+        contentView.transform = CGAffineTransform(translationX: 0, y: view.bounds.height)
+        UIView.animate(withDuration: 0.5, animations: {
+            self.contentView.transform = .identity
+            self.confettiView.startConfettiAnimation()
+        })
+    }
+    
+    // MARK: AnimateHide
+    private func animateHide() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.contentView.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.height)
+        }) { _ in
+            self.dismiss(animated: false, completion: nil)
+        }
     }
 }
 
