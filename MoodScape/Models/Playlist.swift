1487076528f108667a95d7998499c78a1ab3684e
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-17 23:28:56 +0200

    Playlist model implemented

---
+++ MoodScape/Models/Playlist.swift
@@ -0,0 +1,17 @@
+//
+//  Playlist.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 17.09.24.
+//
+
+import UIKit
+
+struct Playlist {
+    let id: String
+    let name: String
+    let color: UIColor
+    let date: String
+    let emotions: [String]
+    let spotifyURL: URL
+}
