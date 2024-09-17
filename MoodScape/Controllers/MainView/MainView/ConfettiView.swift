c7998107c701a71f92cfd5ba5620fbf74f9d4524
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-17 19:36:42 +0200

    ConfettiView (for animation after a new playlist was created) implemented

---
+++ MoodScape/Controllers/MainView/MainView/ConfettiView.swift
@@ -0,0 +1,58 @@
+//
+//  ConfettiView.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 17.09.24.
+//
+
+import UIKit
+
+class ConfettiView: UIView {
+    private var emitterLayer: CAEmitterLayer?
+
+    func startConfettiAnimation() {
+        emitterLayer = CAEmitterLayer()
+        emitterLayer?.emitterPosition = CGPoint(x: self.bounds.width / 2, y: 0)
+        emitterLayer?.emitterShape = .line
+        emitterLayer?.emitterSize = CGSize(width: self.bounds.width, height: 1)
+
+        let confettiColors: [UIColor] = [
+            .red, .blue, .green, .yellow, .orange, .purple, .cyan, .magenta
+        ]
+        
+        let confettiCells = confettiColors.map { makeConfettiCell(color: $0) }
+        
+        emitterLayer?.emitterCells = confettiCells
+        self.layer.addSublayer(emitterLayer!)
+    }
+
+    private func makeConfettiCell(color: UIColor) -> CAEmitterCell {
+        let cell = CAEmitterCell()
+        cell.contents = circleImage(color: color).cgImage // Use a circle shape as confetti
+        cell.birthRate = 100
+        cell.lifetime = 5.0
+        cell.velocity = 200
+        cell.velocityRange = 50
+        cell.emissionRange = .pi
+        cell.scale = 0.2
+        cell.scaleRange = 0.1
+        cell.yAcceleration = 150
+        return cell
+    }
+
+    // Helper to create a small circle UIImage
+    private func circleImage(color: UIColor) -> UIImage {
+        let diameter: CGFloat = 10.0
+        let size = CGSize(width: diameter, height: diameter)
+        
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fillEllipse(in: CGRect(origin: .zero, size: size))
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
