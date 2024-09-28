c5e47f71e3cd08c8003e59ace8927cc04d3f24d3
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-28 23:35:24 +0200

    ContactViewController: view changed; message label with animation implemented, buttons for email and feedback added, mail compose added

--- MoodScape/Controllers/SettingsView/ContactView.swift
+++ MoodScape/Controllers/SettingsView/ContactView.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import MessageUI // For sending emails
 
-class ContactViewController: ProfileBaseView {
+class ContactViewController: ProfileBaseView, MFMailComposeViewControllerDelegate {
+    
+    // MARK: - Properties
     
     private let contactLabel: UILabel = {
         let settingsLabel = UILabel()
@@ -19,7 +22,39 @@ class ContactViewController: ProfileBaseView {
         return settingsLabel
     }()
     
-    private let label: UILabel = {
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 21, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let emailButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Contact Support", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let feedbackButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Send Feedback", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let appLabel: UILabel = {
         let settingsLabel = UILabel()
         settingsLabel.text = "MoodScape"
         settingsLabel.font = UIFont.systemFont(ofSize: 28, weight: .bold)
@@ -29,27 +64,107 @@ class ContactViewController: ProfileBaseView {
         return settingsLabel
     }()
     
-    // - MARK: ViewDidLoad
+    private let versionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "App Version 1.0.0"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupConstraints()
     }
     
-    // - MARK: SetupView
-    private func setupView() {
-        view.addSubview(contactLabel)
-        view.addSubview(label)
+    // MARK: ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        messageLabel.startTypingAnimation(label: messageLabel, text: "You can contact our support at support@moodscape.io or provide feedback at feedback@moodscape.io.", typingSpeed: 0.05) {}
     }
     
-    // - MARK: SetupConstraints
+    // MARK: - SetupView
+    private func setupView() {
+        view.addSubview(contactLabel)
+        view.addSubview(appLabel)
+        view.addSubview(messageLabel)
+        view.addSubview(emailButton)
+        view.addSubview(feedbackButton)
+        view.addSubview(versionLabel)
+        
+        emailButton.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
+        feedbackButton.addTarget(self, action: #selector(sendFeedback), for: .touchUpInside)
+    }
+    
+    // MARK: SetupConstraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             contactLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             contactLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            appLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            appLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            messageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            messageLabel.topAnchor.constraint(equalTo: contactLabel.bottomAnchor, constant: 100),
+            messageLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            emailButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300),
+            emailButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            emailButton.widthAnchor.constraint(equalToConstant: 200),
+            emailButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            feedbackButton.topAnchor.constraint(equalTo: emailButton.bottomAnchor, constant: 20),
+            feedbackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            feedbackButton.widthAnchor.constraint(equalToConstant: 200),
+            feedbackButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            versionLabel.bottomAnchor.constraint(equalTo: appLabel.bottomAnchor, constant: 10),
+            versionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+    }
+    
+    // MARK: - SendEmail
+    @objc private func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposeVC = MFMailComposeViewController()
+            mailComposeVC.mailComposeDelegate = self
+            mailComposeVC.setToRecipients(["support@moodscape.io"]) // Replace with your support email
+            mailComposeVC.setSubject("Support Request")
+            mailComposeVC.setMessageBody("Hello, I need help with...", isHTML: false)
+            
+            present(mailComposeVC, animated: true, completion: nil)
+        } else {
+            // Show alert if mail services are not available
+            let alert = UIAlertController(title: "Mail Services Unavailable", message: "Please set up an email account to send mail.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: SendFeedback
+    @objc private func sendFeedback() {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposeVC = MFMailComposeViewController()
+            mailComposeVC.mailComposeDelegate = self
+            mailComposeVC.setToRecipients(["feedback@moodscape.io"]) // TODO: REPLACE
+            mailComposeVC.setSubject("MoodScape Feedback")
+            mailComposeVC.setMessageBody("Here is my feedback...", isHTML: false)
+            
+            present(mailComposeVC, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Mail Services Unavailable", message: "Please set up an email account to send feedback.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - MailCompose Delegate
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
