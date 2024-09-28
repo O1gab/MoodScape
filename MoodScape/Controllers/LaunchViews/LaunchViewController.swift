b9ad2cdff536c499af4cd8b56845027d40bd621b
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-29 01:06:46 +0200

    LaunchView: preloadViews function implemented, marks added

--- MoodScape/Controllers/LaunchViews/LaunchViewController.swift
+++ MoodScape/Controllers/LaunchViews/LaunchViewController.swift
@@ -9,6 +9,7 @@ import Firebase
 
 class LaunchViewController: StartBaseView {
 
+    // MARK: - Properties
     private let name: UILabel = {
         let name = UILabel()
         name.text = "MoodScape"
@@ -19,14 +20,18 @@ class LaunchViewController: StartBaseView {
         return name
     }()
     
-    // - MARK: ViewDidLoad
+    private var startView: StartViewController?
+    private var mainView: MainTabBarController?
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        preloadViews()
         setupView()
         setupConstraints()
     }
     
-    // - MARK: ViewWillAppear
+    // MARK: ViewWillAppear
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
@@ -35,42 +40,50 @@ class LaunchViewController: StartBaseView {
         }
     }
     
-    // - MARK: SetupView
+    // MARK: - PreloadViews
+    private func preloadViews() {
+        startView = StartViewController()
+        startView?.loadViewIfNeeded()
+        mainView = MainTabBarController()
+        mainView?.loadViewIfNeeded()
+    }
+    
+    // MARK: SetupView
     private func setupView() {
         appLabel.alpha = 0
         view.addSubview(name)
     }
     
-    // - MARK: SetupConstraints
+    // MARK: SetupConstraints
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             name.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             name.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
         ])
     }
-
-    // - MARK: AnimateName
-    private func animateName() {
-        UIView.animate(withDuration: 3.0, animations: {
-            self.name.alpha = 0.0
-        }) { _ in
-            self.checkAuthentication()
-        }
-    }
     
-    // - MARK: CheckAuthentication
+    // MARK: - CheckAuthentication
     private func checkAuthentication() {
         if Auth.auth().currentUser == nil {
-            let startView = StartViewController()
+            guard let startView = startView else { return }
             startView.modalTransitionStyle = .crossDissolve
             startView.modalPresentationStyle = .fullScreen
             self.present(startView, animated: true, completion: nil)
         
          } else {
-            let mainView = MainTabBarController()
+            guard let mainView = mainView else { return }
             mainView.modalTransitionStyle = .crossDissolve
             mainView.modalPresentationStyle = .fullScreen
             self.present(mainView, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - AnimateName
+    private func animateName() {
+        UIView.animate(withDuration: 3.0, animations: {
+            self.name.alpha = 0.0
+        }) { _ in
+            self.checkAuthentication()
         }
     }
 }
