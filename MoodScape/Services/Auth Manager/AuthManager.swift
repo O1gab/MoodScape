5a44932278c4e6d16b198bd899111637fd1f116e
Olga Batiunia <olgabatiunia@gmail.com>
2024-12-09 14:54:09 +0100

    AuthManager, AppleSignInManager implemented, Sign in with Apple functionality enabled

---
+++ MoodScape/Services/Auth Manager/AuthManager.swift
@@ -0,0 +1,48 @@
+//
+//  AuthManager.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 09.12.24.
+//
+import FirebaseAuth
+import AuthenticationServices
+
+class AuthManager {
+    static let shared = AuthManager()
+
+    private init() {}
+
+    func appleAuth(
+        _ appleIDCredential: ASAuthorizationAppleIDCredential,
+        nonce: String?
+    ) async throws -> AuthDataResult? {
+        guard let nonce = nonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+
+        guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Unable to fetch identity token")
+            return nil
+        }
+
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return nil
+        }
+
+        let credentials = OAuthProvider.appleCredential(withIDToken: idTokenString,
+                                                        rawNonce: nonce,
+                                                        fullName: appleIDCredential.fullName)
+
+        do {
+            return try await authenticateUser(credentials: credentials)
+        } catch {
+            print("FirebaseAuthError: appleAuth(appleIDCredential:nonce:) failed. \(error)")
+            throw error
+        }
+    }
+
+    private func authenticateUser(credentials: AuthCredential) async throws -> AuthDataResult? {
+        return try await Auth.auth().signIn(with: credentials)
+    }
+}
