49974bc4084b681d56b1ef86a989c07399613a70
Olga Batiunia <olgabatiunia@gmail.com>
2024-12-12 23:36:26 +0100

    marks added

--- MoodScape/Extensions/UILabelExtensions.swift
+++ MoodScape/Extensions/UILabelExtensions.swift
@@ -15,6 +15,7 @@ extension UILabel {
         let fullText = text
         label.text = ""
         
+        
         var index = 0
         timer = Timer.scheduledTimer(withTimeInterval: typingSpeed, repeats: true) { [weak self] timer in
             guard let self = self else { return }
