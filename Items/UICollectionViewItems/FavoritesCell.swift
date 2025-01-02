3b4bd90e3fc162e702a8123947b8376a7d92328f
Olga Batiunia <olgabatiunia@gmail.com>
2025-01-02 17:49:58 +0100

    marks added

---
+++ Items/UICollectionViewItems/FavoritesCell.swift
@@ -0,0 +1,92 @@
+//
+//  FavoritesCell.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 03.09.24.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        setupConstraints()
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.75),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 15),
+            subtitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 4),
+            subtitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 4),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
+    func configure(with imageUrl: String, title: String, subtitle: String) {
+        imageView.loadImage(from: imageUrl)
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+    }
+}
+
+extension UIImageView {
+    func loadImage(from url: String) {
+        guard let url = URL(string: url) else { return }
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }.resume()
+    }
+}
---
+++ Items/UICollectionViewItems/GeneratedPlaylistCell.swift
@@ -0,0 +1,66 @@
+//
+//  GeneratedPlaylistCell.swift
+//  MoodScape
+//
+//  Created by Olga Batiunia on 18.09.24.
+//
+
+import Foundation
+
+class GeneratedPlaylistCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let colorView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 25
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(colorView)
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            colorView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            colorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            colorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            colorView.heightAnchor.constraint(equalToConstant: 150),
+            
+            titleLabel.topAnchor.constraint(equalTo: colorView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ])
+    }
+    
+    // MARK: Init
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    func configure(with playlist: Playlist) {
+        colorView.backgroundColor = playlist.color
+        
+        let maxLength = 40
+        let truncatedName = playlist.name.count > maxLength ?
+            String(playlist.name.prefix(maxLength)) + "..." : 
+            playlist.name
+        
+        titleLabel.text = truncatedName
+    }
+}
