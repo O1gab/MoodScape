aa451ca712e3d3e387bdbd964d96c1ca1822f9eb
Olga Batiunia <olgabatiunia@gmail.com>
2025-01-06 19:36:42 +0100

    SpotifyLinkView ui view implemented

---
+++ Items/UIView/SpotifyLinkView.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class SpotifyLinkView: UIView {
+    private let spotifyIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "spotify.logo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let songLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        layer.cornerRadius = 10
+        
+        addSubview(spotifyIcon)
+        addSubview(songLabel)
+        
+        NSLayoutConstraint.activate([
+            spotifyIcon.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            spotifyIcon.centerYAnchor.constraint(equalTo: centerYAnchor),
+            spotifyIcon.widthAnchor.constraint(equalToConstant: 24),
+            spotifyIcon.heightAnchor.constraint(equalToConstant: 24),
+            
+            songLabel.leadingAnchor.constraint(equalTo: spotifyIcon.trailingAnchor, constant: 10),
+            songLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            songLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure(with songInfo: String) {
+        songLabel.text = songInfo
+    }
+}
